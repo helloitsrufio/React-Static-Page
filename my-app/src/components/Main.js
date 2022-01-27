8d1bd9752194a292a1a76c65e6fea39f202cf4c6
Ruth Reed <ruthreed.dev@gmail.com>
2022-01-26 23:14:44 -0700

    Finish styling for simple react page.

--- my-app/src/components/Main.js
+++ my-app/src/components/Main.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 export default function Main(){
     return(
-        <h1>This is the Main component.</h1>
+        <section className='mainSection'>
+            <h1>Fun facts about React</h1>
+            <ul>
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Facebook</li>
+                <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+        </section>
     )
     
 }
